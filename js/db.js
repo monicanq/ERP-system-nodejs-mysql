741a3e764ac4102bff2409822911a6bf0407baa2
monicanq <monicanq@gmail.com>
2019-09-13 22:43:56 +0200

    changes to style and database connection

--- js/db.js
+++ js/db.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const http = require('http');
 
 // Create connection
 const db = mysql.createConnection({
@@ -9,14 +10,21 @@ const db = mysql.createConnection({
     database : 'neptury'
 });
 
-// Connect
+// Connect to the database
 db.connect((err) => {
     if (err) throw err; // not connected!
     console.log('MySql Connected...');
 });
 
-const app = express();
-
-app.listen('3006', () => {
-    console.log('Server started on port 3006');
+//Start the Server
+const http = require ('http');
+// const fs = require ('fs'); //Uncomment if we want to stream content
+let server = http.createServer((req, res) =>{
+  console.log('Request was made: '+ req.url);
+  res.writeHead(200, {'Content-Type' : 'text/html'});
+  // const myReadStream = fs.createReadStream(__dirname + '/filename.html', 'utf8'); //uncomment if we want to stream content
+  // myReadStream.pipe(res);
+  res.end('Serve r started on port 3006'); //Comment if we are streaming content
 });
+server.listen(3006,'127.0.0.1');
+console.log('Server up and running');
