d6d5c8226051f20b601257229a8490e427a04596
monicanq <monicanq@gmail.com>
2019-09-14 22:00:56 +0200

    Added all the files I forgot in the last commit

--- js/myscript.js
+++ js/myscript.js
@@ -4,29 +4,29 @@ const passwordPattern = /^[0-9]{4}$/
 const msg = document.querySelector('.loginMessage');
 
 
-form.addEventListener('submit', e =>{
-  e.preventDefault();
-  const username = form.Username.value;
-  const password = form.password.value;
-  if (usernamePattern.test(username)){
-    console.log('The username is correct, let me validate it');
-    msg.innerHTML = 'The username is correct, let me validate it.';
-  } else{
-    console.log('The username is not valid');
-    msg.innerHTML = 'The username is not valid';
-  }
-  if (passwordPattern.test(password)){
-    console.log('Correct password');
-  } else {
-    msg.innerHTML += ' The password is not correct';
-  }
-})
+//This will be handled by the backend
+// form.addEventListener('submit', e =>{
+//   e.preventDefault();
+//   const username = form.username.value;
+//   const password = form.password.value;
+//   if (usernamePattern.test(username)){
+//     console.log('The username is correct, let me validate it');
+//     // msg.innerHTML = 'The username is correct, let me validate it.';
+//   } else{
+//     console.log('The username is not valid');
+//     // msg.innerHTML = 'The username is not valid';
+//   }
+//   if (passwordPattern.test(password)){
+//     console.log('Correct password');
+//   } else {
+//     msg.innerHTML += ' The password is not correct';
+//   }
+// })
 
-
-form.Username.addEventListener('keyup', e =>{
+form.username.addEventListener('keyup', e =>{
   if (usernamePattern.test(e.target.value)){
-    form.Username.setAttribute('class', 'correct');
+    form.username.setAttribute('class', 'correct');
   } else{
-    form.Username.setAttribute('class', 'wrong');
+    form.username.setAttribute('class', 'wrong');
   }
 })
